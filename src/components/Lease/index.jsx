6dc27136c18c6f2382d634b3dad0e27c37084d2b
notsearch1 <notsearch1@gmail.com>
2022-06-05 22:45:37 +0200

    mise à jour du responsive + page lease ok

--- src/components/Lease/index.jsx
+++ src/components/Lease/index.jsx
@@ -3,10 +3,11 @@ import Header from "../Header";
 import { useFetch } from "../../utils/hooks";
 import Carousel from "../Carousel/Carousel";
 import "../../styles/Lease.css";
+import "../../styles/Dropdown.css";
 import { useParams } from "react-router-dom";
 import { Loader } from "../../utils/Atoms.jsx";
 import Stars from "../Stars";
-import { useState } from "react";
+
 
 
 
@@ -14,6 +15,11 @@ function Lease() {
   const { id } = useParams();
   const { data, error, isLoading } = useFetch(`../../../datas/datas.json`);
   const dataArr = []
+  const openDesc = (e) => {
+    e.target.classList.toggle("rotate");
+    e.target.closest(".dropdown-title").nextSibling.classList.toggle("block");
+    e.target.closest(".dropdown-title").classList.toggle("updateMargin");
+  };
   for (let i = 0; i < data.length; i++) {
     if (data[i].id === id) {
       dataArr.push(data[i])
@@ -25,7 +31,6 @@ function Lease() {
       <span className="error">Un problème est survenu lors du téléchargement des images</span>
     );
   }
-
   return (
     <div>
       <Header />
@@ -37,7 +42,19 @@ function Lease() {
           <div className="information-section">
             <h1 className="lease-title">{dataArr[0].title}</h1>
             <span className="lease-location">{dataArr[0].location}</span>
-
+              <div className="tag">
+                <ul className="tag-list">
+                  {dataArr.map(({ tags }) => {
+                    return tags.map((tag) => {
+                      return (
+                        <li className="tag-item" key={tag}>
+                          {tag}
+                        </li>
+                      );
+                    });
+                  })}
+                </ul>
+              </div>
             <div className="title-container">
               <ul className="star-container">
                 <Stars number={dataArr[0].rating} />
@@ -53,25 +70,50 @@ function Lease() {
                 />
               </figure>
             </div>
-            <div className="tag">
-              <ul className="tag-list">
-                {dataArr.map(({ tags }) => {
-                  return tags.map((tag) => {
-                    return (
-                      <li className="tag-item" key={tag}>
-                        {tag}
-                      </li>
-                    );
-                  });
-                })}
-              </ul>
+            <section className="dropdown-section">
+              <div className="dropdown-container">
+              <div className="dropdown-title">
+                <h2 className="dropdown-title-text">Description</h2>
+                <span>
+                  <i
+                    className="fa-solid fa-angle-down"
+                    onClick={(e) => openDesc(e)}
+                  ></i>
+                </span>
+              </div>
+              {dataArr.map(({ description }) => {
+                return <p className="dropdown-text">{description}</p>  
+                }
+              )}
+              </div>
+              <div className="dropdown-container">
+            <div className="dropdown-title">
+                <h2 className="dropdown-title-text">Equipements</h2>
+                <span>
+                  <i
+                    className="fa-solid fa-angle-down"
+                    onClick={(e) => openDesc(e)}
+                    ></i>
+                </span>
             </div>
-          </div>
-        </div>
-      )}
+            <ul className="equipement-list dropdown-text">
+              {dataArr.map(({ equipments }) => {
+                return equipments.map((equipment) => {
+                  return (
+                    <li className="equipement-item" key={equipment}>
+                      {equipment}
+                    </li>
+                  );
+                });
+              })}
+            </ul>
+              </div>
+            </section>
+        </div> 
+      </div>
+  )}
       <Footer />
     </div>
-  );
-}
+)}
 
 export default Lease;
